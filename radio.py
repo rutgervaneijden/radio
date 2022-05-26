6a848bbd03db53d6461307f9380951c99c728038
rutgervaneijden <63955794+rutgervaneijden@users.noreply.github.com>
2022-05-26 19:40:59 +0200

    Update radio.py

--- radio.py
+++ radio.py
@@ -2,23 +2,16 @@ import json
 import requests
 import subprocess
 import time
-URL = 'http://icecast.omroep.nl/3fm-sb-mp3'
-PASSWORD = 'radio'
 while True:
-    try:
-        print(subprocess.getoutput('pgrep vlc'))
-        response = requests.get('http://:radio@127.0.0.1:8080/requests/status.json')
-        response = json.loads(response.text)
-        t0 = response['time']
+    if subprocess.getoutput('pgrep vlc') == "":
+        subprocess.Popen(['cvlc','--intf','http','--http-password','radio','--extraintf','telnet','--telnet-password','radio'])
         time.sleep(3)
-        response = requests.get('http://:radio@127.0.0.1:8080/requests/status.json')
-        response = json.loads(response.text)
-        t1 = response['time']
-        state = response['state']
+    else:
+        t0 = requests.get('http://:radio@127.0.0.1:8080/requests/status.json').json()['time']
+        time.sleep(3)
+        t1 = requests.get('http://:radio@127.0.0.1:8080/requests/status.json').json()['time']
+        state =  requests.get('http://:radio@127.0.0.1:8080/requests/status.json').json()['state']
         if t0 == t1 and state != "paused":
             requests.get('http://:radio@127.0.0.1:8080/requests/status.json?command=pl_empty')
-            requests.get(f'http://:radio@127.0.0.1:8080/requests/status.json?command=in_play&input={URL}')
-    except:
-        subprocess.Popen(['cvlc','--intf','http','--http-password',f'{PASSWORD}'])
-        time.sleep(3)
+            requests.get('http://:radio@127.0.0.1:8080/requests/status.json?command=in_play&input=http://icecast.omroep.nl/3fm-sb-mp3')
 done
